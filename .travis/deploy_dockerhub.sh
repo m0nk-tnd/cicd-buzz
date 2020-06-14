bd152246f57accabaeb1835b4a1d6a202bec3fea
tnd_max <mail@tnd27.ru>
2020-06-14 21:43:17 +0300

    fix docker build script 3

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+repo=`echo ${TRAVIS_REPO_SLUG} | cut -f 2 -d /`
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG .
-docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $DOCKER_USER/$repo:$TAG .
+docker push $DOCKER_USER/$repo
